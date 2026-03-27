081dd7a7b8120a390f5d36023e20e53fffb1fbb4
mahadeep14 <pagalamahadeep14@gmail.com>
2026-03-27 20:30:26 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+class Calc{
+    public int add(int a,int b){
+        return a+b;
+    }
+    protected void display(){
+        System.out.println("message");
+    }
+    private void show(){
+                System.out.println("hello");
+    }
+    public void accessPrivate(){
+              show();
+    }
+}
+class Adv extends Calc{
+    public int add(int a,int c){
+        return a+c;
+    }
+  protected void display(){
+        System.out.println("message overrided");
+   }
+   public void show(){
+       System.out.println("not overridden method");//new method which is not overridden because it is private in parent class
+   }
+}
+public class Main{
+      public static void main(String[] args){
+          Calc o = new Calc();
+         
+          System.out.println("sum of 2:"+ o.add(10,78));
+         o.accessPrivate();
+         o.display();
+         Adv p=new Adv();
+         p.display();
+         p.add(7,9);
+         p.show();
+      }
+}
