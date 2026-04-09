3ac1eab478994835f4fdb69f59cd79ee78e2eb07
mahadeep14 <pagalamahadeep14@gmail.com>
2026-04-09 21:17:59 +0530

    Add files via upload

---
+++ packages in java/Main.java
@@ -0,0 +1,8 @@
+import mathoperations.Addition;
+
+public class Main {
+    public static void main(String[] args) {
+        int result = Addition.add(10, 20);
+        System.out.println("Sum = " + result);
+    }
+}
